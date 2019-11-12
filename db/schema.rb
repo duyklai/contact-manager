9b607b8d99166b6897901ba31c09a86f6b4dc57a
Duy Lai <duyklai@gmail.com>
2019-11-11 19:23:33 -0600

    Generate email address and associate with person

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_232854) do
+ActiveRecord::Schema.define(version: 2019_11_12_012105) do
+
+  create_table "email_addresses", force: :cascade do |t|
+    t.string "address"
+    t.integer "person_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "first_name"
