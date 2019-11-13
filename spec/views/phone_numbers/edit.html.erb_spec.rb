945ba9898d214b9e69702471facd44e7dba4e0d5
Duy Lai <duyklai@gmail.com>
2019-11-13 15:07:17 -0600

    Add dynamic titling to new/edit phone (last commit was only email)

--- spec/views/phone_numbers/edit.html.erb_spec.rb
+++ spec/views/phone_numbers/edit.html.erb_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "phone_numbers/edit", type: :view do
+  let(:person) { Person.create(:first_name => "Bob", :last_name => "Smith") }
+  let(:phone_number) { PhoneNumber.create(:contact_id => person.id, :contact_type => "Person", :number => "MyString") }
   before(:each) do
-    @phone_number = assign(:phone_number, PhoneNumber.create!(
-      :number => "MyString",
-      :contact_id => 1
-    ))
+    @phone_number = assign(:phone_number, phone_number)
   end
+  # before(:each) do
+  #   @phone_number = assign(:phone_number, PhoneNumber.create!(
+  #     :number => "MyString",
+  #     :contact_id => 1
+  #   ))
+  # end
 
   it "renders the edit phone_number form" do
     render
@@ -18,4 +23,10 @@ RSpec.describe "phone_numbers/edit", type: :view do
       assert_select "input[name=?]", "phone_number[contact_id]"
     end
   end
+
+  it "shows the contact's name in the title" do
+    render
+    assert_select("h1", text: "Editing Phone Number for #{phone_number.contact.last_name}, #{phone_number.contact.first_name}")
+  end
+
 end
